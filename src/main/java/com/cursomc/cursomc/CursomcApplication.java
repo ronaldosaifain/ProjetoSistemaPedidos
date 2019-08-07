87f6bb076dfe99f5c39c507b27d588dd9221f0c2
Ronaldo Santos <ronaldo100timo@gmail.com>
2019-08-07 16:15:13 -0300

    operação de instanciação

--- src/main/java/com/cursomc/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/cursomc/CursomcApplication.java
@@ -1,15 +1,34 @@
 package com.cursomc.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import com.cursomc.domain.Categoria;
+import com.cursomc.repository.CategoriaRepository;
+
 @SpringBootApplication
 @EntityScan("com.cursomc.domain")
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 
-	public static void main(String[] args) {
+private CategoriaRepository categoriaRepository;	
+	
+	public static void main(String[] args)  {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+	  categoriaRepository.saveAll(Arrays.asList(cat1, cat2));	
+		
+		
 	}
 
 }
