b1958173b10fe62a02b9daf0be3f9ca048362d74
Ronaldo Santos <ronaldo100timo@gmail.com>
2019-08-27 15:29:44 -0300

    Implementando autorizacao

--- src/main/java/com/sispedidos/sispedidos/config/SecurityConfig.java
+++ src/main/java/com/sispedidos/sispedidos/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.sispedidos.domain.security.JWTAuthenticationFilter;
+import com.sispedidos.domain.security.JWTAuthorizationFilter;
 import com.sispedidos.domain.security.JWTUtil;
 
 @Configuration
@@ -58,7 +59,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
