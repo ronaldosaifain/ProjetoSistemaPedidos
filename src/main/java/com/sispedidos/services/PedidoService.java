93145a3edf558bc690370698822bb4a46ba8f24f
Ronaldo Santos <ronaldo100timo@gmail.com>
2019-08-28 00:20:47 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/sispedidos/services/PedidoService.java
+++ src/main/java/com/sispedidos/services/PedidoService.java
@@ -4,16 +4,22 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sispedidos.domain.Cliente;
 import com.sispedidos.domain.ItemPedido;
 import com.sispedidos.domain.PagamentoComBoleto;
 import com.sispedidos.domain.Pedido;
 import com.sispedidos.domain.enums.EstadoPagamento;
+import com.sispedidos.domain.security.UserSS;
 import com.sispedidos.repository.ItemPedidoRepository;
 import com.sispedidos.repository.PagamentoRepository;
 import com.sispedidos.repository.PedidoRepository;
+import com.sispedidos.service.exceptions.AuthorizationException;
 import com.sispedidos.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,22 +27,22 @@ public class PedidoService {
 
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PedidoRepository repo;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
 
@@ -48,21 +54,21 @@ public class PedidoService {
 
 	}
 
-	  @Transactional
+	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
 		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto) {
-		PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();	
-		boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());	
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
-      
+
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		for(ItemPedido ip : obj.getItens()) {
+		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
@@ -71,6 +77,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
-		
+
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
